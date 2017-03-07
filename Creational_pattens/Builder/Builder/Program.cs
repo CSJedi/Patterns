56f184d5683b11de6eaba64a58ea3901eec9d09f
tetiana.zhydeikina.kn.2016@lpnu.ua <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-03-07 21:45:22 +0200

    add realization of Builder pattern

--- Creational_pattens/Builder/Builder/Program.cs
+++ Creational_pattens/Builder/Builder/Program.cs
@@ -1,15 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
 namespace Builder
 {
-    class Program
+    class Product
     {
-        static void Main(string[] args)
+        List<object> parts = new List<object>();
+
+        public void Add(string part)
         {
+            parts.Add(part);
+        }
+    }
+    abstract class Buider
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract void BuildPartC();
+        public abstract Product GetResult();
+
+    }
+
+    class ConcreteBuilder : Buider
+    {
+        Product product = new Product();
+        public override void BuildPartA()
+        {
+            product.Add("Part A");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("Part B");
+        }
+
+        public override void BuildPartC()
+        {
+            product.Add("Part C");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+
+
+    class Director
+    {
+        Buider builder;
+
+        public Director(Buider builder)
+        {
+            this.builder = builder;
+        }
+
+        public void Construct()
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+            builder.BuildPartC();
+        }
+    }
+
+    class Client
+    {
+        void Main()
+        {
+            Buider buider = new ConcreteBuilder();
+            Director director = new Director(buider);
+            director.Construct();
+            Product product = buider.GetResult();
         }
     }
 }
