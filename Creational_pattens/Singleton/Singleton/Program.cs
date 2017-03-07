54ad10819f96bdb07665adb998fbf49527523ef1
tetiana.zhydeikina.kn.2016@lpnu.ua <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-03-07 19:52:51 +0200

    set more understandable realization of Singleton pattern

--- Creational_pattens/Singleton/Singleton/Program.cs
+++ Creational_pattens/Singleton/Singleton/Program.cs
@@ -6,18 +6,46 @@ using System.Threading.Tasks;
 
 namespace Singleton
 {
-    class Singleton
+    class Program
     {
-        private static Singleton instance;
-        private Singleton()
+        static void Main(string[] args)
         {
-            
+            Person person = new Person();
+            person.Launch("999999999999999999999999999");
+            Console.WriteLine(person.IIN.Code);
+
+            person.IIN = IIN.getInstance("888888888888888");
+            Console.WriteLine(person.IIN.Code);
+            Console.ReadKey();
+        }
+    }
+
+    //-------------------------------------------
+    class Person
+    {
+        public IIN IIN { get; set; }
+
+        public void Launch(string code)
+        {
+            this.IIN = IIN.getInstance(code);
+        }
+    }
+
+    //-------------------------------------------
+    class IIN
+    {
+        private static IIN instance;
+        public string Code { get; private set; }
+
+        protected IIN(string code)
+        {
+            this.Code = code;
         }
 
-        public static Singleton getInstance()
+        public static IIN getInstance(string code)
         {
-            if (instance == null)
-                instance = new Singleton();
+            if(instance == null)
+                instance = new IIN(code);
 
             return instance;
         }
