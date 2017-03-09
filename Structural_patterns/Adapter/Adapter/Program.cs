decedc88e89915a2d6ed9d0bbbb1bdb681b79096
tetiana.zhydeikina.kn.2016@lpnu.ua <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-03-09 19:19:56 +0200

    add simple realization of Adapter pattern

---
+++ Structural_patterns/Adapter/Adapter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Adapter
+{
+    class Target
+    {
+        public virtual void Request()
+        {
+            
+        }
+    }
+    class Client
+    {
+        public void Request(Target target)
+        {
+            target.Request();
+        }
+    }
+
+    class Adapter: Target
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+    class Adaptee 
+    {
+        public void SpecificRequest()
+        {
+            
+        }
+    }
+}
